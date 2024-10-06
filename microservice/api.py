6c009419606af7386cb0d82781f2b228b13940a5
Jimmy Chu <jchu@meilleursagents.com>
2024-10-06 15:55:39 +0200

    feat(workflow): enable API to receive components and validate it

--- microservice/api.py
+++ microservice/api.py
@@ -1,6 +1,16 @@
-from pydantic import BaseModel
-from fastapi import FastAPI, Depends
+from enum import Enum
+from typing import Union
+
+
+from collections import defaultdict
+
+from fastapi.encoders import jsonable_encoder
+from fastapi import Depends, FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, field_validator
 from sqlmodel import Session
+
 from microservice.db.engine import create_tables, get_session
 from microservice.db.models import Workflow
 
@@ -11,19 +21,106 @@ app = FastAPI()
 def start_db():
     create_tables()
 
-# TODO: add list of components with `type` and `settings` fields to the request
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    reformatted_error_messages = defaultdict(list)
+    for validations_errors in exc.errors():
+        error_message = validations_errors.get("msg", "")
+        invalid_field_raw = validations_errors.get("loc", "")
+
+        invalid_field = (
+            invalid_field_raw[1:]
+            if invalid_field_raw[0] in ("body", "query", "path")
+            else invalid_field_raw
+        )
+        invalid_field_path = ".".join([str(x) for x in invalid_field])
+
+        reformatted_error_messages[invalid_field_path].append(error_message)
+
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder(
+            {"detail": "Invalid request", "errors": reformatted_error_messages}
+        ),
+    )
+
+
+class ComponentTypeEnum(str, Enum):
+    IMPORT = "import"
+    SHADOW = "shadow"
+    CROP = "crop"
+    EXPORT = "export"
+
+
+class ComponentSchema(BaseModel):
+    type: ComponentTypeEnum
+    settings: dict[str, Union[int, float, str, bool]] | None = None
 
 
 class WorkflowSchema(BaseModel):
     name: str
+    components: list[ComponentSchema] = []
+
+    @field_validator("components")
+    def validate_components_uniqueness(cls, components):
+        unique_components_types = set()
+        duplicated_components_types = set()
+
+        for c in components:
+            if c.type in unique_components_types:
+                duplicated_components_types.add(c.type)
+            unique_components_types.add(c.type)
+
+        if duplicated_components_types:
+            duplicated_components_types_str = ", ".join(
+                [c.value for c in duplicated_components_types]
+            )
+            raise ValueError(f"Duplicated types: {duplicated_components_types_str}")
+
+        return components
+
+    @field_validator("components")
+    def validate_components_import_export_position(cls, components):
+        import_index = next(
+            (i for i, c in enumerate(components) if c.type == ComponentTypeEnum.IMPORT),
+            None,
+        )
+        export_index = next(
+            (i for i, c in enumerate(components) if c.type == ComponentTypeEnum.EXPORT),
+            None,
+        )
+
+        # neither import nor export components are present
+        if import_index is None and export_index is None:
+            return components
+
+        if export_index is not None and export_index != len(components) - 1:
+            if import_index is not None and import_index != 0:
+                raise ValueError(
+                    "'import' must be at the start and 'export' must be at the end of components"
+                )
+            raise ValueError("'export' must be at the end of components")
+
+        if import_index is not None and import_index != 0:
+            raise ValueError("'import' must be at the start of components")
+
+        return components
+
+    @field_validator("components")
+    def validate_components_settings(cls, components):
+        has_settings = [c.settings is not None for c in components]
+
+        if any(has_settings) and not all(has_settings):
+            raise ValueError(
+                "settings must either be present or missing for all components"
+            )
+
+        return components
 
 
 @app.post("/workflow")
-def create_workflow(
-        request: WorkflowSchema,
-        session: Session = Depends(get_session)
-
-):
+def create_workflow(request: WorkflowSchema, session: Session = Depends(get_session)):
     # TODO: validate and store components
     workflow_db = Workflow(name=request.name)
     session.add(workflow_db)
--- tests/test_create_workflow.py
+++ tests/test_create_workflow.py
@@ -1,14 +1,16 @@
-from unittest import TestCase
+import uuid
+
 from assertpy import assert_that
 from fastapi.testclient import TestClient
-from microservice.db.engine import get_test_session, get_session
+from parameterized import parameterized
+from unittest import TestCase
 
 from microservice.api import app
+from microservice.db.engine import get_test_session, get_session
 from microservice.db.models import Workflow
 
 
 class TestAPI(TestCase):
-
     @classmethod
     def setUpClass(cls) -> None:
         app.dependency_overrides[get_session] = get_test_session
@@ -18,12 +20,138 @@ class TestAPI(TestCase):
     def tearDown(cls) -> None:
         app.dependency_overrides.clear()
 
+    def test_workflow_with_invalid_components_type_should_return_422(self):
+        payload = {"name": "component_name", "components": [{"type": "zoom"}]}
+
+        response = self.client.post("/workflow/", json=payload)
+        errors = response.json()["errors"]
+
+        assert_that(response.status_code).is_equal_to(422)
+        assert_that(errors["components.0.type"]).is_equal_to(
+            ["Input should be 'import', 'shadow', 'crop' or 'export'"]
+        )
+
+    def test_workflow_with_invalid_settings_should_return_422(self):
+        payload = {
+            "name": "component_name",
+            "components": [{"type": "crop", "settings": {"a": {"b": 11}}}],
+        }
+        response = self.client.post("/workflow/", json=payload)
+        errors = response.json()["errors"]
+
+        assert_that(response.status_code).is_equal_to(422)
+        assert_that(errors["components.0.settings.a.int"]).is_equal_to(
+            ["Input should be a valid integer"]
+        )
+        assert_that(errors["components.0.settings.a.str"]).is_equal_to(
+            ["Input should be a valid string"]
+        )
+        assert_that(errors["components.0.settings.a.float"]).is_equal_to(
+            ["Input should be a valid number"]
+        )
+        assert_that(errors["components.0.settings.a.bool"]).is_equal_to(
+            ["Input should be a valid boolean"]
+        )
+
+    def test_workflow_with_duplicated_types_should_return_422(self):
+        payload = {
+            "name": "component_name",
+            "components": [{"type": "crop"}, {"type": "crop"}],
+        }
+
+        response = self.client.post("/workflow/", json=payload)
+        errors = response.json()["errors"]
+
+        assert_that(response.status_code).is_equal_to(422)
+        assert_that(errors["components"]).is_equal_to(
+            ["Value error, Duplicated types: crop"]
+        )
+
+    @parameterized.expand(
+        [
+            [
+                "import_export_mispositioned",
+                {
+                    "name": "component_name",
+                    "components": [
+                        {"type": "crop"},
+                        {"type": "import"},
+                        {"type": "export"},
+                        {"type": "shadow"},
+                    ],
+                },
+                [
+                    "Value error, 'import' must be at the start and 'export' must be at the end of components"
+                ],
+            ],
+            [
+                "import_not_at_start",
+                {
+                    "name": "test",
+                    "components": [
+                        {"type": "shadow"},
+                        {"type": "import"},
+                    ],
+                },
+                ["Value error, 'import' must be at the start of components"],
+            ],
+            [
+                "export_not_at_end",
+                {
+                    "name": "component_name",
+                    "components": [
+                        {"type": "export"},
+                        {"type": "shadow"},
+                    ],
+                },
+                ["Value error, 'export' must be at the end of components"],
+            ],
+        ]
+    )
+    def test_workflow_with_invalid_import_export_components_should_return_422(
+        self, name, payload, error_message
+    ):
+        response = self.client.post("/workflow/", json=payload)
+        errors = response.json()["errors"]
+
+        assert_that(response.status_code).is_equal_to(422)
+        assert_that(errors["components"]).is_equal_to(error_message)
+
+    def test_workflow_with_inconsistent_settings_should_return_422(self):
+        payload = {
+            "name": "component_name",
+            "components": [
+                {"type": "crop", "settings": {"key": 11}},
+                {"type": "export"},
+            ],
+        }
+
+        response = self.client.post("/workflow/", json=payload)
+        errors = response.json()["errors"]
+
+        assert_that(response.status_code).is_equal_to(422)
+        assert_that(errors["components"]).is_equal_to(
+            [
+                "Value error, settings must either be present or missing for all components"
+            ]
+        )
+
+    def test_workflow_with_valid_compoenent_payload_should_return_200(self):
+        payload = {
+            "name": "name",
+            "components": [
+                {"type": "import"},
+                {"type": "crop"},
+            ],
+        }
+        response = self.client.post("/workflow/", json=payload)
+
+        assert_that(response.status_code).is_equal_to(200)
+
     def test_should_create_workflow_with_name(self):
         given_workflow = {"name": "test"}
 
-        response = self.client.post(
-            "/workflow/", json=given_workflow
-        )
+        response = self.client.post("/workflow/", json=given_workflow)
 
         assert_that(response.status_code).is_equal_to(200)
         workflow_id = response.json()
@@ -31,8 +159,6 @@ class TestAPI(TestCase):
 
         # without a GET endpoint, we look directly in the db
         with next(get_test_session()) as session:
-            wf = session.get(Workflow, workflow_id)
+            wf = session.get(Workflow, uuid.UUID(workflow_id))
             assert_that(str(wf.id)).is_equal_to(workflow_id)
             assert_that(wf.name).is_equal_to(given_workflow["name"])
-
-    # TODO: test components support and validation
